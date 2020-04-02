d2e6912de0dab023af62a4a2aba2e4dcf5d4fcf9
atfb84 <atfb84@mst.edu>
2020-04-02 15:31:03 -0600

    Initialize repository with file structure and virtual environment

---
+++ src/app/bot.py
@@ -0,0 +1,10 @@
+'''
+Author(s): Adam Forestier
+Last Updated: April 2, 2020
+Description: bot.py contains the driver code for an Instagram bot
+'''
+
+# Libraries
+from selenium import webdriver
+
+class InstaBot
