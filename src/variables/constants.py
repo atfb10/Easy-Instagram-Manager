790f9ec6d599e556a6e0528e829b481d4209ada6
atfb84 <atfb84@mst.edu>
2020-04-10 20:22:07 -0600

    Add constant variables to project

---
+++ src/variables/constants.py
@@ -0,0 +1,96 @@
+'''
+Author(s): Adam Forestier
+Last Updated: April 10, 2020
+Description: contants.py contains all unchanging variables for the project
+'''
+
+# Variables
+CLIMB = 'climb'
+MTN = 'mountain'
+SKI = 'ski'
+HIKE = 'hike'
+CAMP = 'camp'
+SPORT = 'sportclimbing'
+TRAD = 'tradclimbing'
+BOULDER = 'bouldering'
+BC_SKI = 'backcountryski' 
+CO = 'colorado'
+WY = 'wyoming'
+WA = 'washington'
+CA = 'california'
+NV = 'nevada'
+OR = 'oregon'
+
+# List objects
+base_ae_hashtags = [
+    'wanderlust',
+    'nature',
+    'adventure',
+    'travel',
+    'explore',
+    'wilderness'
+]
+
+base_climbing_hashtags = [
+    'climbing',
+    'climb',
+    'rockclimbing',
+    'climber',
+    'climbingday',
+    'climbingpartner'
+]
+
+base_ski_hashtags = [
+    'skiing',
+    'ski',
+    'snow',
+    'shred',
+    'powder',
+    'skiresort'
+]
+
+base_hiking_hashtags = [
+    'hiking',
+    'hike',
+    'backpacking',
+    'trails',
+    'trail',
+    'walking'
+]
+
+camping_hashtags = [
+    'camping',
+    'camp',
+    'tent',
+    'campground',
+    'nightunderthestars',
+    'campinglife'
+]
+
+spec_trad_climbing_hashtags = [
+    'trad',
+    'tradclimbing',
+    'tradclimb',
+    'tradisrad'
+]
+
+spec_sport_climbing_hashtags = [
+    'sportclimb',
+    'sportclimbing',
+    'cragging',
+    ''
+]
+
+spec_bouldering_hashtags = [
+    'boulder',
+    'bouldering',
+    'crashpads',
+    'tryhard'
+]
+
+spec_bc_ski_hashtags = [
+    'skimo',
+    'backcountry',
+    'backcountryskiing',
+    'backcountryski'
+]
