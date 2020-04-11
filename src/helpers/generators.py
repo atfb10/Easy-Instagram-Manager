47ea6c9af3ec02786cb4849ae8ac987ebd2f7754
atfb84 <atfb84@mst.edu>
2020-04-10 20:28:59 -0600

    Random # generator for specific hashtag lists

--- src/helpers/generators.py
+++ src/helpers/generators.py
@@ -5,4 +5,13 @@ Description: generators.py contains the functions to handle random number genera
 '''
 
 # Libraries
-import math
+from random import randint
+
+def select_specific_hashtags():
+    '''
+    arguments: none
+    returns: randomly generated number between 0 and 3
+    description select_specific_hashtags() generates and returns a number between 0-3
+    to select a hashtags from specific hashtag lists
+    '''
+    return randint(0, 3)
